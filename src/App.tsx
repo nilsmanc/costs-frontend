2a0aa27ff9e756ebef3134ade6dc32143f0e4b5d
nilsmanc <etereaiberia@gmail.com>
2022-09-11 12:05:16 +0300

    effector

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
+import { useStore } from 'effector-react'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { AuthPage } from './components/AuthPage/AuthPage'
 import { Header } from './components/Header/Header'
+import { $auth } from './context/auth'
 
 function App() {
+  const isLoggedIn = useStore($auth)
+
   return (
     <div className='App'>
       <Header />
+      <Router>
+        <Routes>
+          <Route
+            path='/'
+            element={isLoggedIn ? <Navigate to='/costs' /> : <AuthPage type='login' />}
+          />
+          <Route
+            path='/registration'
+            element={isLoggedIn ? <Navigate to='/costs' /> : <AuthPage type='registration' />}
+          />
+          <Route
+            path='/login'
+            element={isLoggedIn ? <Navigate to='costs' /> : <AuthPage type='login' />}
+          />
+          <Route path='/costs' element={isLoggedIn ? <h1>Costs</h1> : <Navigate to='/login' />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
