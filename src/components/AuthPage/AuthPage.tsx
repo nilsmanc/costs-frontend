d8920acba390ae286306ff24b64df0b75798deef
nilsmanc <etereaiberia@gmail.com>
2022-09-11 12:56:52 +0300

    alert, spinner, login endpoint

--- src/components/AuthPage/AuthPage.tsx
+++ src/components/AuthPage/AuthPage.tsx
@@ -1,8 +1,32 @@
+import { MutableRefObject, useRef, useState } from 'react'
 import { Link } from 'react-router-dom'
+import { AuthClient } from '../../api/authClient'
 import './styles.css'
 
 export const AuthPage = ({ type }: { type: 'login' | 'registration' }) => {
+  const [spinner, setSpinner] = useState(false)
+  const usernameRef = useRef() as MutableRefObject<HTMLInputElement>
+  const passwordRef = useRef() as MutableRefObject<HTMLInputElement>
+
   const currentAuthTitle = type === 'login' ? 'Войти' : 'Регистрация'
+
+  const handleLogin = async (username: string, password: string) => {
+    if (!username || !password) {
+      return
+    }
+    const result = await AuthClient.login(username, password)
+  }
+
+  const handleRegistration = async (username: string, password: string) => {
+    if (!username || !password) {
+      return
+    }
+
+    if (password.length < 4) {
+      return
+    }
+    const result = await AuthClient.login(username, password)
+  }
   return (
     <div className='container'>
       <h1>{currentAuthTitle}</h1>
--- src/context/auth.ts
+++ src/context/auth.ts
@@ -2,6 +2,6 @@ import { createDomain } from 'effector'
 
 const auth = createDomain()
 
-const setAuth = auth.createEvent<boolean>()
+export const setAuth = auth.createEvent<boolean>()
 
 export const $auth = auth.createStore<boolean>(false).on(setAuth, (_, value) => value)
